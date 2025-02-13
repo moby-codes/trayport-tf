c957b5a0fc350fab155a1fb35731391f60bf92d8
Mubashar Ali <64266350+moby-codes@users.noreply.github.com>
2025-02-13 16:49:05 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "s3_buckets" {
     product_name = string
     versioning   = optional(bool, false)
     object_lock  = optional(bool, false)
-    lock_mode    = optional(string, )
+    lock_mode    = optional(string)
     retention    = optional(number)
   }))
 }
